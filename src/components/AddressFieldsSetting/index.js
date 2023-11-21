dc9bc3fa347fa47365e49dd989fe1635ec5ddbb8
carlosg-ordering <carlos.g@ordering.co>
2023-11-21 11:32:08 -0600

    [UPDATE] v1.40.4

--- src/components/AddressFieldsSetting/index.js
+++ src/components/AddressFieldsSetting/index.js
@@ -3,82 +3,198 @@ import PropTypes from 'prop-types'
 import { useApi } from '../../contexts/ApiContext'
 import { useToast, ToastType } from '../../contexts/ToastContext'
 import { useLanguage } from '../../contexts/LanguageContext'
-import { useValidationFields } from '../../contexts/ValidationsFieldsContext'
+import { useSession } from '../../contexts/SessionContext'
 
 /**
  * Component to manage address field behavior without UI component
  */
 export const AddressFieldsSetting = (props) => {
   const {
-    UIComponent
+    UIComponent,
+    code
   } = props
 
   const [ordering] = useApi()
   const [, { showToast }] = useToast()
   const [, t] = useLanguage()
-  const [, { loadValidationFields }] = useValidationFields()
+  const [{ token }] = useSession()
 
-  const [addressFieldsState, setAddressFieldsState] = useState({ fields: [], loading: false, error: null })
+  const [validationFieldState, setValidationFieldState] = useState({ fields: [], loading: false, error: null })
+  const [originaState, setOriginalState] = useState({ fields: [], loading: false, error: null })
+  const [sitesState, setSitesState] = useState({ loading: true, sites: [], error: null })
   const [actionState, setActionState] = useState({ loading: false, error: null })
+  const [selectedSiteId, setSelectedSiteId] = useState(null)
 
   /**
    * Method to get the address fields from API
    */
-  const getAddressFields = async () => {
+  const getAddressFieldsBySite = async () => {
     try {
-      setAddressFieldsState({ ...addressFieldsState, loading: true })
-      const conditons = [
-        {
-          attribute: 'validate',
-          value: 'address'
+      setValidationFieldState({ ...validationFieldState, loading: true })
+      const requestOptions = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
         }
-      ]
-      const { content: { result, error } } = await ordering.validationFields().where(conditons).get()
+      }
+      const response = await fetch(`${ordering.root}/validation_field_sites`, requestOptions)
+      const { result, error } = await response.json()
       if (!error) {
-        const filteredResult = result.filter(
-          field => field.code !== 'city_dropdown_option' && field.code !== 'name' &&
-          field.code !== 'lastname' && field.code !== 'mobile_phone' &&
-          field.code !== 'email' && field.code !== 'middle_name' && field.code !== 'second_lastname'
-        )
-        setAddressFieldsState({ fields: filteredResult, loading: false })
+        let filteredResult = []
+        if (code === 'address') {
+          filteredResult = result.filter(
+            field => field?.validation_field?.validate === 'address' && field?.validation_field?.code !== 'city_dropdown_option' && field?.validation_field?.code !== 'name' &&
+            field?.validation_field?.code !== 'lastname' && field?.validation_field?.code !== 'mobile_phone' &&
+            field?.validation_field?.code !== 'email' && field?.validation_field?.code !== 'middle_name' && field?.validation_field?.code !== 'second_lastname'
+          )
+        } else {
+          filteredResult = result.filter(field => field?.validation_field?.validate === 'card')
+        }
+        setValidationFieldState({ fields: filteredResult, loading: false })
       }
     } catch (err) {
-      setAddressFieldsState({ ...addressFieldsState, loading: false, error: [err.message] })
+      setValidationFieldState({ ...validationFieldState, loading: false, error: [err.message] })
     }
   }
 
   /**
-   * Method to update the address fields setting from API
+   * Method to update the checkout fields setting from API
    * @param {Number} fieldId selected field id
+   * @param {Number} siteId site id
    * @param {Object} changes changes
    */
-  const handleChangeFieldSetting = async (fieldId, changes) => {
+  const handleChangeFieldSetting = async (fieldId, siteId, changes) => {
     try {
       showToast(ToastType.Info, t('LOADING', 'Loading'))
       setActionState({ ...actionState, loading: true })
-      const { content: { result, error } } = await ordering.validationFields(fieldId).save(changes)
-
-      if (!error) {
+      const content = await updateCheckoutField(fieldId, siteId, changes)
+      if (!content.error) {
         setActionState({ loading: false, error: null })
         showToast(ToastType.Success, t('FIELD_SAVED', 'Field saved'))
-        const fields = addressFieldsState.fields.filter(field => {
-          if (field.id === fieldId) {
-            Object.assign(field, result)
+        const fields = validationFieldState.fields.map(field => {
+          if (field.site_id === siteId && field.validation_field_id === fieldId) {
+            return {
+              ...field,
+              ...changes
+            }
           }
-          return true
+          return field
         })
-        setAddressFieldsState({ ...addressFieldsState, fields: fields })
-        loadValidationFields && loadValidationFields()
+        setValidationFieldState({ ...validationFieldState, fields })
       } else {
-        setActionState({ loading: false, error: result })
+        setActionState({ loading: false, error: content.result })
       }
     } catch (err) {
       setActionState({ loading: false, error: [err.message] })
     }
   }
 
+  /**
+   * Method to get the address fields from API
+   */
+  const getAddressFields = async () => {
+    try {
+      setOriginalState({ ...originaState, loading: true })
+      const conditons = [
+        {
+          attribute: 'validate',
+          value: code ?? 'address'
+        }
+      ]
+      const { content: { result, error } } = await ordering.validationFields().where(conditons).get()
+      if (!error) {
+        let filteredResult = result
+        if (code === 'address') {
+          filteredResult = result.filter(
+            field => field.code !== 'city_dropdown_option' && field.code !== 'name' &&
+            field.code !== 'lastname' && field.code !== 'mobile_phone' &&
+            field.code !== 'email' && field.code !== 'middle_name' && field.code !== 'second_lastname'
+          )
+        }
+        setOriginalState({ fields: filteredResult, loading: false })
+      }
+    } catch (err) {
+      setOriginalState({ ...originaState, loading: false, error: [err.message] })
+    }
+  }
+
+  /**
+   * Method to get the sites from API
+   */
+  const handleGetSitesList = async () => {
+    try {
+      const requestOptions = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      }
+      const response = await fetch(`${ordering.root}/sites`, requestOptions)
+      const { error, result } = await response.json()
+      if (!error) {
+        setSitesState({
+          loading: false,
+          sites: result,
+          error: null
+        })
+      } else {
+        setSitesState({
+          ...sitesState,
+          loading: false,
+          error: result
+        })
+      }
+    } catch (error) {
+      setSitesState({
+        ...sitesState,
+        loading: false,
+        error: [error.message]
+      })
+    }
+  }
+
+  const updateCheckoutField = async (fieldId, siteId, changes, isResult) => {
+    const requestOptions = {
+      method: isResult ? 'POST' : 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify(changes)
+    }
+    const response = await fetch(`${ordering.root}/validationfields/${fieldId}/sites/${siteId}`, requestOptions)
+    const content = await response.json()
+    if (content?.error) throw new Error(content.result)
+    if (isResult) return content.result
+    return content
+  }
+
+  const updateCheckoutFields = async () => {
+    try {
+      setActionState({ ...actionState, loading: true, isPost: true })
+      const content = await Promise.all(originaState?.fields.map(field => updateCheckoutField(field?.id, selectedSiteId, { enabled: true, ...(field?.code === 'email' && { required: true }) }, true)))
+      setValidationFieldState({ ...validationFieldState, fields: [...validationFieldState.fields, ...content] })
+      setActionState({ ...actionState, loading: false, isPost: false })
+    } catch (err) {
+      setActionState({ loading: false, error: [err.message], isPost: false })
+    }
+  }
+
+  useEffect(() => {
+    if (selectedSiteId) {
+      const fieldsBySite = validationFieldState?.fields.filter(field => field.site_id === selectedSiteId)
+      if (fieldsBySite?.length === 0) {
+        updateCheckoutFields()
+      }
+    }
+  }, [selectedSiteId])
+
   useEffect(() => {
     getAddressFields()
+    handleGetSitesList()
+    getAddressFieldsBySite()
   }, [])
 
   return (
@@ -86,8 +202,13 @@ export const AddressFieldsSetting = (props) => {
       {UIComponent && (
         <UIComponent
           {...props}
-          addressFieldsState={addressFieldsState}
+          validationFieldState={validationFieldState}
           handleChangeFieldSetting={handleChangeFieldSetting}
+          sitesState={sitesState}
+          selectedSiteId={selectedSiteId}
+          setSelectedSiteId={setSelectedSiteId}
+          actionState={actionState}
+
         />
       )}
     </>
--- src/components/CheckoutFieldsSetting/index.js
+++ src/components/CheckoutFieldsSetting/index.js
@@ -4,7 +4,6 @@ import { useSession } from '../../contexts/SessionContext'
 import { useApi } from '../../contexts/ApiContext'
 import { useToast, ToastType } from '../../contexts/ToastContext'
 import { useLanguage } from '../../contexts/LanguageContext'
-import { useValidationFields } from '../../contexts/ValidationsFieldsContext'
 
 export const CheckoutFieldsSetting = (props) => {
   const {
@@ -15,19 +14,81 @@ export const CheckoutFieldsSetting = (props) => {
   const [{ token }] = useSession()
   const [, { showToast }] = useToast()
   const [, t] = useLanguage()
-  const [, { loadValidationFields }] = useValidationFields()
 
   const hideSettingList = ['city_dropdown_option', 'address', 'zipcode', 'address_notes']
 
   const [checkoutFieldsState, setCheckoutFieldsState] = useState({ fields: [], loading: false, error: null })
+  const [originaState, setOriginalState] = useState({ fields: [], loading: false, error: null })
+  const [sitesState, setSitesState] = useState({ loading: true, sites: [], error: null })
   const [actionState, setActionState] = useState({ loading: false, error: null })
+  const [selectedSiteId, setSelectedSiteId] = useState(null)
+
+  /**
+   * Method to get the sites from API
+   */
+  const handleGetSitesList = async () => {
+    try {
+      const requestOptions = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      }
+      const response = await fetch(`${ordering.root}/sites`, requestOptions)
+      const { error, result } = await response.json()
+      if (!error) {
+        setSitesState({
+          loading: false,
+          sites: result,
+          error: null
+        })
+      } else {
+        setSitesState({
+          ...sitesState,
+          loading: false,
+          error: result
+        })
+      }
+    } catch (error) {
+      setSitesState({
+        ...sitesState,
+        loading: false,
+        error: [error.message]
+      })
+    }
+  }
+
+  /**
+   * Method to get the checkout fields from API
+   */
+  const getCheckoutFieldsBySites = async () => {
+    try {
+      setCheckoutFieldsState({ ...checkoutFieldsState, loading: true })
+      const requestOptions = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      }
+      const response = await fetch(`${ordering.root}/validation_field_sites`, requestOptions)
+      const content = await response.json()
+      if (!content.error) {
+        const checkoutFields = content.result.filter(field => field?.validation_field?.validate === 'checkout' && !hideSettingList.includes(field?.validation_field?.code))
+        setCheckoutFieldsState({ fields: checkoutFields, loading: false })
+      }
+    } catch (err) {
+      setCheckoutFieldsState({ ...checkoutFieldsState, loading: false, error: [err.message] })
+    }
+  }
 
   /**
    * Method to get the checkout fields from API
    */
   const getCheckoutFields = async () => {
     try {
-      setCheckoutFieldsState({ ...checkoutFieldsState, loading: true })
+      setOriginalState({ ...originaState, loading: true })
       const requestOptions = {
         method: 'GET',
         headers: {
@@ -51,43 +112,37 @@ export const CheckoutFieldsSetting = (props) => {
             validationF.push(item)
           }
         })
-        setCheckoutFieldsState({ fields: validationF, loading: false })
+        setOriginalState({ fields: validationF, loading: false })
       }
     } catch (err) {
-      setCheckoutFieldsState({ ...checkoutFieldsState, loading: false, error: [err.message] })
+      setOriginalState({ ...originaState, loading: false, error: [err.message] })
     }
   }
 
   /**
    * Method to update the checkout fields setting from API
    * @param {Number} fieldId selected field id
+   * @param {Number} siteId site id
    * @param {Object} changes changes
    */
-  const handleChangeCheckoutFieldSetting = async (fieldId, changes) => {
+  const handleChangeCheckoutFieldSetting = async (fieldId, siteId, changes) => {
     try {
       showToast(ToastType.Info, t('LOADING', 'Loading'))
       setActionState({ ...actionState, loading: true })
-      const requestOptions = {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify(changes)
-      }
-      const response = await fetch(`${ordering.root}/checkoutfields/${fieldId}`, requestOptions)
-      const content = await response.json()
+      const content = await updateCheckoutField(fieldId, siteId, changes)
       if (!content.error) {
         setActionState({ loading: false, error: null })
         showToast(ToastType.Success, t('FIELD_SAVED', 'Field saved'))
-        const fields = checkoutFieldsState.fields.filter(field => {
-          if (field.id === fieldId) {
-            Object.assign(field, content.result)
+        const fields = checkoutFieldsState.fields.map(field => {
+          if (field.site_id === siteId && field.validation_field_id === fieldId) {
+            return {
+              ...field,
+              ...changes
+            }
           }
-          return true
+          return field
         })
-        setCheckoutFieldsState({ ...checkoutFieldsState, fields: fields })
-        loadValidationFields && loadValidationFields()
+        setCheckoutFieldsState({ ...checkoutFieldsState, fields })
       } else {
         setActionState({ loading: false, error: content.result })
       }
@@ -96,8 +151,46 @@ export const CheckoutFieldsSetting = (props) => {
     }
   }
 
+  const updateCheckoutField = async (fieldId, siteId, changes, isResult) => {
+    const requestOptions = {
+      method: isResult ? 'POST' : 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify(changes)
+    }
+    const response = await fetch(`${ordering.root}/validationfields/${fieldId}/sites/${siteId}`, requestOptions)
+    const content = await response.json()
+    if (content?.error) throw new Error(content.result)
+    if (isResult) return content.result
+    return content
+  }
+
+  const updateCheckoutFields = async () => {
+    try {
+      setActionState({ ...actionState, loading: true, isPost: true })
+      const content = await Promise.all(originaState?.fields.map(field => updateCheckoutField(field?.id, selectedSiteId, { enabled: true, ...(field?.code === 'email' && { required: true }) }, true)))
+      setCheckoutFieldsState({ ...checkoutFieldsState, fields: [...checkoutFieldsState.fields, ...content] })
+      setActionState({ ...actionState, loading: false, isPost: false })
+    } catch (err) {
+      setActionState({ loading: false, error: [err.message], isPost: false })
+    }
+  }
+
+  useEffect(() => {
+    if (selectedSiteId) {
+      const fieldsBySite = checkoutFieldsState?.fields.filter(field => field.site_id === selectedSiteId)
+      if (fieldsBySite?.length === 0) {
+        updateCheckoutFields()
+      }
+    }
+  }, [selectedSiteId])
+
   useEffect(() => {
     getCheckoutFields()
+    handleGetSitesList()
+    getCheckoutFieldsBySites()
   }, [])
 
   return (
@@ -105,8 +198,12 @@ export const CheckoutFieldsSetting = (props) => {
       {UIComponent && (
         <UIComponent
           {...props}
+          sitesState={sitesState}
           checkoutFieldsState={checkoutFieldsState}
           handleChangeCheckoutFieldSetting={handleChangeCheckoutFieldSetting}
+          selectedSiteId={selectedSiteId}
+          setSelectedSiteId={setSelectedSiteId}
+          actionState={actionState}
         />
       )}
     </>
